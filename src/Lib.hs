b4d4a5d8226c490663d616047c424e74b6b225a4
Dario Bertini <berdario@gmail.com>
2016-12-20 07:42:15 +0000

    Naive solution

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,13 +4,6 @@ module Lib
 
 import           Data.List (sortBy)
 
-comp GT _ _ _ = GT
-comp LT _ _ _ = LT
-comp EQ _ [] [] = EQ
-comp EQ previous (x:_) [] = compare x previous
-comp EQ previous [] (y:_) = compare previous y
-comp EQ _ (x:xs) (y:ys) = comp (compare x y) (max x y) xs ys
-
 biggest :: [Int] -> String
-biggest = concat . sortBy (flip $ comp EQ '0') . map show
+biggest = concat . sortBy (flip compare) . map show
 
